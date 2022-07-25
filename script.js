b80ce9e51797eb958fd64da88031ff6236dcfefc
Aigul <sharipova.aig@gmail.com>
2022-07-25 19:00:28 -0400

    function click

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const button = document.querySelector('#btn');
+const button = document.querySelector("#btn");
 button.addEventListener("click", calculateAmount);
 
 function calculateAmount(e) {
   e.preventDefault();
-  const bill = document.querySelector('#bill').value;
-  const people = document.querySelector('#people').value;
-  const tip = document.querySelector('#tip').value;
+  const bill = document.querySelector("#bill").value;
+  const people = document.querySelector("#people").value;
+  const tip = document.querySelector("#tip").value;
 
 if (bill === "" || people === "") {
   alert('Error')};
 
 let amountPerPerson = bill / people;
 let tipPerPerson = (bill * tip) / people;
-let totalSum = amountPerPerson + amountTip;
+let totalSum = amountPerPerson + tipPerPerson;
 
 amountPerPerson = amountPerPerson.toFixed(2);
 tipPerPerson = tipPerPerson.toFixed(2);
 totalSum = totalSum.toFixed(2);
 
-document.querySelector('#dividedBill').textContent = amountPerPerson;
-document.querySelector('#ddividedTip').textContent = tipPerPerson;
-document.querySelector('#billAndTip').textContent = totalSum ;
+document.querySelector("#dividedBill").textContent = amountPerPerson;
+document.querySelector("#dividedTip").textContent = tipPerPerson;
+document.querySelector("#billAndTip").textContent = totalSum ;
 }
