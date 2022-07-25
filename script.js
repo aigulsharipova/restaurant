7db0e921c7bddce1f064e99be7e1e2422b99a6cd
Aigul <sharipova.aig@gmail.com>
2022-07-25 07:56:35 -0400

    let tip

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ button.addEventListener('click', calculateAmount);
 function calculateAmount(e) {
   e.preventDefault();
   const bill = document.querySelector('#bill').value;
-  const people = document.querySelector('#tip').value;
+  const people = document.querySelector('#people').value;
+  const tip = document.querySelector('#tip').value;
 if (bill === "" || people === "") {
   alert('Error')}
+
+  //1 How many will pay 1 people
 let amountPerPerson = bill/people;
+let amountTip = (bill * tip) / people;
 }
