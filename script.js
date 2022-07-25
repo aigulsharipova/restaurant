4989bbd5b176fd1493c731bc00f83d1cc7149c1b
Aigul <sharipova.aig@gmail.com>
2022-07-25 07:46:57 -0400

    preventDefault

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const button = document.querySelector('#calcutale');
 button.addEventListener('click', calculateAmount);
 
-function calculateAmount() {
+function calculateAmount(e) {
+  e.preventDefault();
   const bill = document.querySelector('#bill').value;
   const people = document.querySelector('#tip').value;
 if (bill === "" || people === "") {
   alert('Error')}
+
 }
