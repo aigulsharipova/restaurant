a1aa6e819bc396ffcd34e4abafeb957f0be5d49e
Aigul <sharipova.aig@gmail.com>
2022-07-25 18:47:54 -0400

    toFixed

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const button = document.querySelector('#calcutale');
-button.addEventListener('click', calculateAmount);
+button.addEventListener("click", calculateAmount);
 
 function calculateAmount(e) {
   e.preventDefault();
   const bill = document.querySelector('#bill').value;
   const people = document.querySelector('#people').value;
   const tip = document.querySelector('#tip').value;
-if (bill === "" || people === "") {
-  alert('Error')}
 
-  //1 How many will pay 1 people
-let amountPerPerson = bill/people;
-let amountTip = (bill * tip) / people;
+if (bill === "" || people === "") {
+  alert('Error')};
+
+let amountPerPerson = bill / people;
+let tipPerPerson = (bill * tip) / people;
 let totalSum = amountPerPerson + amountTip;
 
-document.querySelector('#dividedBuill').textContent = amountPerPerson;
-document.querySelector('#ddividedTip').textContent = amountTip;
+amountPerPerson = amountPerPerson.toFixed(2);
+tipPerPerson = tipPerPerson.toFixed(2);
+totalSum = totalSum.toFixed(2);
+
+document.querySelector('#dividedBill').textContent = amountPerPerson;
+document.querySelector('#ddividedTip').textContent = tipPerPerson;
 document.querySelector('#billAndTip').textContent = totalSum ;
 }
