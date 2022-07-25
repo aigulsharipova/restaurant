f418256a8fd791c8e7cc1b84767779c8f83fcfa4
Aigul <sharipova.aig@gmail.com>
2022-07-25 07:35:18 -0400

    if empty

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ button.addEventListener('click', calculateAmount);
 function calculateAmount() {
   const bill = document.querySelector('#bill').value;
   const people = document.querySelector('#tip').value;
+if (bill === "" || people === "") {
+  alert('Error')}
 }
